22917f5aa4a75ffe39818d9c71a1dc02e68c3545
Aman Sandhu <asandhu5656@gmail.com>
2024-10-31 17:12:59 -0400

    All components built and some css styles tested

---
+++ src/ChangeMode.jsx
@@ -0,0 +1,11 @@
+function ChangeMode() {
+  return (
+    <div>
+      <button className="change-mode-btn">☀️/🌙</button>
+    </div>
+  );
+}
+
+export default ChangeMode;
+
+// TODO: allow the user to change the theme to dark mode or light mode. ALSO automatically match the users browser mode type.
